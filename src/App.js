378b18c58ee4e403824b4e8df279f5c2fe9ac6e8
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-12-20 18:00:04 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
-  return <div></div>;
+  const [step, setStep] = useState(1);
+  // const [test, setTest] = useState({ name: "jonas" });
+
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+    // step = step + 1;
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+      // setTest({ name: "fred" });
+    }
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step{step}:{messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => alert("previous")}
+              onMouseEnter={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => alert("Next")}
+              onMouseEnter={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
